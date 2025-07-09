a4be374c3b783af2c3d468dba3f4cdf305ce25b8
Hossein Eskandari <eskandary.hoseyn@gmail.com>
2025-07-09 19:05:04 +0300

    feat: add Firebase email/password authentication logic

---
+++ app/src/main/java/ui/screens/login/LoginScreen.kt
@@ -0,0 +1,65 @@
+package ui.screens.login
+
+import android.R.attr.text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun LoginScreen(
+    onNavigateToRegister: () -> Unit,
+    onLoginClick: (String, String) -> Unit
+) {
+
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        OutlinedTextField(
+            label = { Text("Email") },
+            value = email,
+            onValueChange = { email = it },
+            singleLine = true,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        OutlinedTextField(
+            label = { Text("Password") },
+            value = password,
+            onValueChange = { password = it },
+            visualTransformation = PasswordVisualTransformation(),
+            singleLine = true,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Button(onClick = { onLoginClick(email, password) }) {
+            Text("Login")
+        }
+        TextButton(onClick = onNavigateToRegister) {
+            Text("Don't have an account? Register")
+        }
+    }
+}
