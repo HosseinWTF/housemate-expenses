03b9b3d7c3166d046484f90264453de2e50879d9
Hossein Eskandari <eskandary.hoseyn@gmail.com>
2025-07-13 16:24:05 +0300

    createRoom and getUserRooms functions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     implementation(libs.androidx.credentials)
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
+    implementation(libs.androidx.room.runtime.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
